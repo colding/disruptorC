8fffb8dd7c86208d26bfe8896bea4786793af2b9
Jules Colding <jcolding@gmail.com>
2013-03-24 23:42:33 +0100

    Removed a single '*' that had somehow sneaked into the license text

--- test/performance.c
+++ test/performance.c
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2012 by Jules Colding <jcolding@gmail.com>.
+ *    Copyright (C) 2012-2013, Jules Colding <jcolding@gmail.com>.
  *
  *    All Rights Reserved.
  */
@@ -8,23 +8,24 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- *
- * 1) Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution.
- *
- * 3) The name of the author may not be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
+ * 
+ *     (1) Redistributions of source code must retain the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer.
+ * 
+ *     (2) Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *     
+ *     (3) The name of the author may not be used to endorse or
+ *     promote products derived from this software without specific
+ *     prior written permission.
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL * THE AUTHOR BE LIABLE FOR ANY
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
  * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
